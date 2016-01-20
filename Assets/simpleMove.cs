e205d4b363901f4812e3bdac64b3889def0a1556
unknown <c3ljaask@PELIKONE6-PC.wpk.tpu.fi>
2016-01-20 14:52:55 +0200

    LaurinSandBox

---
+++ Assets/simpleMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleMove : MonoBehaviour {
+
+    public bool enabled = false;
+    public float movementSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enabled)
+        {
+            transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/tubeTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tubeTrigger : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject.Find("GameSceneObjects").GetComponent<simpleMove>().enabled = true;
+    }
+}
