14d8a826897ce387eba823a65ffcba4f6706d1ed
Mika Damsten <mika.damsten@gmail.com>
2016-01-20 12:22:58 +0200

    alku commit

---
+++ Assets/scripts/rottamovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rottamovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Rigidbody2D.AddForce(Vector2.right);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 5), ForceMode2D.Impulse);
+        }
+	}
+}
