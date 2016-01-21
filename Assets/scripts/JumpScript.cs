43ce8f9ff7a7e7212fe04218b522993b9bfe5353
Mika Damsten <mika.damsten@gmail.com>
2016-01-21 16:05:56 +0200

    1st jump commit

---
+++ Assets/scripts/JumpScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScript : MonoBehaviour {
+
+    //public GameObject Camera = GameObject.Find("MainCamera");
+    public Vector2 direction;
+    public float force;
+    Rottamovement rottamovement;
+    GameObject Camera;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        Camera = GameObject.Find("Main Camera");
+        //Rottamovement rottamovement = Camera.GetComponent<Rottamovement>();
+        rottamovement = Camera.GetComponent<Rottamovement>();
+
+        //Camera = GameObject.FindGameObjectWithTag("MainCamera");
+        //direction = rottamovement.jumpDirection;
+        //force = rottamovement.jumpForce;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direction = rottamovement.jumpDirection;
+        force = rottamovement.jumpForce;
+	}
+
+    public void Jump() {
+
+        //force = Rottamovement.jumpForce;
+        //direction = Rottamovement.jumpDirection;
+
+        //force = rottamovement.jumpForce;
+        //Debug.Log("jump force", force);
+        //force = Camera.GetComponent<jumpForce>();
+        //direction = Camera.GetComponent<jumpScript>.jumpDirection();
+        //this<Rigidbody2D>().AddForce(direction, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+        //Debug.Log("accessed");
+    }
+}
--- Assets/scripts/rottamovement.cs
+++ Assets/scripts/rottamovement.cs
@@ -1,36 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class rottamovement : MonoBehaviour {
+public class Rottamovement : MonoBehaviour {
 
     Vector2 rat;
+    Vector2 mouse;
+    GameObject Rat;
+    public Vector2 jumpDirection;
+    public float jumpForce;
+    //GameObject player = GameObject.Find("rotta");
+    JumpScript jumpScript;
+
+    //GameObject.FindGameObjectWithTag("MainCamera");
     //rat = GameObject.Find("rotta").transform.position;
-    Vector2 mouse = Input.mousePosition;
+    
 	// Use this for initialization
     void Awake() {
 
     }
 
 	void Start() {
-        rat = GameObject.Find("rotta").transform.position;
+
+
+        Rat = GameObject.FindGameObjectWithTag("Player");
+        jumpScript = Rat.GetComponent<JumpScript>();
+
 	}
 	
 	// Update is called once per frame
 	void Update() {
 
-        Debug.DrawLine(rat, mouse, Color.red);
+            //GetComponent.FindWithTag("Player");
+            rat = GameObject.Find("rotta").transform.position;
+           
+            //mouse = GetComponent<Camera>().WorldToScreenPoint(Input.mousePosition);
+            mouse = GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+            Debug.DrawLine(rat, mouse, Color.red);
+
         /*if (Input.GetMouseButtonDown(0)){
             OnMouseUp();
             //Vector2 pos = GetComponent<Camera>().transform.position;
             //Vector2 dir = (this.transform.position - GetComponent<Camera>().transform.position).normalized;
             //Debug.DrawLine (pos, pos + dir * 10, Color.red, Mathf.Infinity);
             
-        }
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            //Rigidbody2D.AddForce(Vector2.right);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 5), ForceMode2D.Impulse);
         }*/
+        if (Input.GetMouseButtonDown(0))
+        {
+            jumpDirection = mouse-rat;
+            jumpForce = 5.0f;
+            //Debug.Log("clicked");
+            //Rigidbody2D.AddForce(Vector2.right);
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 5), ForceMode2D.Impulse);
+            //jumpScript jump = GetComponent<jumpScript>();
+            //jump.Jump();
+            //Rat.GetComponent<JumpScript>().Jump();
+            jumpScript.Jump();
+        }
 
 	}
     /*void OnMouseUp()
