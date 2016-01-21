f8645fb2f978d6a4f8fee54a5d0e3eebd7ca44b4
Mika Damsten <mika.damsten@gmail.com>
2016-01-21 11:08:24 +0200

    2nd commit

--- Assets/scripts/rottamovement.cs
+++ Assets/scripts/rottamovement.cs
@@ -3,17 +3,45 @@ using System.Collections;
 
 public class rottamovement : MonoBehaviour {
 
+    Vector2 rat;
+    //rat = GameObject.Find("rotta").transform.position;
+    Vector2 mouse = Input.mousePosition;
 	// Use this for initialization
-	void Start () {
+    void Awake() {
 
+    }
+
+	void Start() {
+        rat = GameObject.Find("rotta").transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
+	void Update() {
+
+        Debug.DrawLine(rat, mouse, Color.red);
+        /*if (Input.GetMouseButtonDown(0)){
+            OnMouseUp();
+            //Vector2 pos = GetComponent<Camera>().transform.position;
+            //Vector2 dir = (this.transform.position - GetComponent<Camera>().transform.position).normalized;
+            //Debug.DrawLine (pos, pos + dir * 10, Color.red, Mathf.Infinity);
+            
+        }
+        /*if (Input.GetMouseButtonDown(0))
         {
             //Rigidbody2D.AddForce(Vector2.right);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 5), ForceMode2D.Impulse);
-        }
+        }*/
+
 	}
+    /*void OnMouseUp()
+    {
+        Ray screenToPointer = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit;
+
+        if (GetComponent<BoxCollider2D>().RaycastHit2D(screenToPointer, out hit, Mathf.Infinity))
+        {
+            Vector2 theVector = (transform.position - hit.point).normalized;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(theVector.x * 10, theVector.y * 10);
+        }
+    }*/
 }
